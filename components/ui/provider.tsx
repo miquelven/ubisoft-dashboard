c54c0415d1043d9a2b01b7e1d830f9b3c5d0a402
Miquelven Silva <miquelven.silva@gmail.com>
2026-02-05 10:04:26 -0300

    feat(ui): wrap ColorModeProvider with SettingsProvider

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -2,11 +2,14 @@
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode"
+import { SettingsProvider } from "./settings"
 
 export function Provider(props: ColorModeProviderProps) {
   return (
     <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
+      <SettingsProvider>
+        <ColorModeProvider {...props} />
+      </SettingsProvider>
     </ChakraProvider>
   )
 }
