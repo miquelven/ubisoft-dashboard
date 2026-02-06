52ceb4d94f705550de885ff95815c555d4b73f47
Miquelven Silva <miquelven.silva@gmail.com>
2026-02-06 11:42:04 -0300

    refactor(ui): use ComponentProps for ColorModeProvider type

--- components/ui/color-mode.tsx
+++ components/ui/color-mode.tsx
@@ -3,11 +3,10 @@
 import type { IconButtonProps } from '@chakra-ui/react';
 import { ClientOnly, IconButton, Skeleton } from '@chakra-ui/react';
 import { ThemeProvider, useTheme } from 'next-themes';
-import type { ThemeProviderProps } from 'next-themes';
 import * as React from 'react';
 import { LuMoon, LuSun } from 'react-icons/lu';
 
-export type ColorModeProviderProps = ThemeProviderProps;
+export type ColorModeProviderProps = React.ComponentProps<typeof ThemeProvider>;
 
 export function ColorModeProvider(props: ColorModeProviderProps) {
   return (
